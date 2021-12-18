4b45be42862107f5c6b78fba6e174169b4c3033f
Paul Woelfel <paul@woelfel.at>
2021-12-18 12:34:50 +0100

    Add pwa settings

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,9 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#1976d2" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
