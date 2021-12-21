13d6800c288862442ce63096c19be1f9ef2c85f9
Paul Woelfel <paul@woelfel.at>
2021-12-21 21:46:50 +0100

    Add about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,50 @@
-import { Typography } from '@mui/material';
+import { Box, Paper, Typography } from '@mui/material';
 
 export default function About() {
   return (
-    <div>
-      <Typography>About</Typography>
-    </div>
+    <Paper sx={{ p: 2, m: 2 }}>
+      <Typography variant="h3" gutterBottom>
+        About
+      </Typography>
+      <Typography>
+        Diese Webseite zeigt Hydranten im Raum Neusiedl am See an, um diese im
+        Einsatzfall einfach zu lokalisieren.
+      </Typography>
+      <Typography variant="h4" gutterBottom>
+        Impressum
+      </Typography>
+      <Typography variant="h5">
+        <b>Für den Inhalt verantwortlich</b>
+      </Typography>
+      <Typography>
+        Feuerwehr Neusiedl am See
+        <br />
+        A-7100 Neusiedl am See, Satzgasse 9<br />
+        Tel: +43 (0) 2167 / 2250
+        <br />
+        Fax:+43 (0) 2167 / 2250 4<br />
+        email: verwaltung [at] ff-neusiedlamsee [dot] at
+        <br />
+        <a
+          href="http://www.ff-neusiedlamsee.at/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          http://www.ff-neusiedlamsee.at/
+        </a>
+      </Typography>
+      <Typography variant="h5">Urheberrechte</Typography>
+      <Typography>
+        Texte, Bilder und Grafiken unterliegen dem Schutz des Urheberrechts und
+        anderen Schutzgesetzen, soweit nicht anders angegeben.
+        <br />
+      </Typography>
+      <Typography variant="h5">Haftung</Typography>
+      <Typography>
+        Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung
+        für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten
+        sind ausschließlich deren Betreiber verantwortlich.
+      </Typography>
+    </Paper>
   );
 }
