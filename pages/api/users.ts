aa94876d335602bda668e1a11b161830ff88932a
Paul Woelfel <paul@woelfel.at>
2021-12-22 13:54:00 +0100

    Fix authorization

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { UserRecord } from 'firebase-admin/lib/auth/user-record';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import adminRequired from '../../server/adminRequired';
 import firebaseAdmin from '../../server/firebase/admin';
 
 const listUsers = async (): Promise<UserRecord[]> => {
@@ -11,30 +12,6 @@ const listUsers = async (): Promise<UserRecord[]> => {
 export interface UsersResponse {
   users: UserRecord;
 }
-
-const adminRequired = async (
-  req: NextApiRequest,
-  res: NextApiResponse<any>
-) => {
-  const { authorization } = req.headers;
-  if (!authorization) {
-    res.status(401).json({ error: 'Unauthorized' });
-    return false;
-  }
-  if (authorization.indexOf(`Bearer `) < 0) {
-    res.status(403).json({ error: 'Bearer token required' });
-    return false;
-  }
-  const token = authorization.replace('Bearer ', '');
-  try {
-    const decodedToken = await firebaseAdmin.auth().verifyIdToken(token);
-    console.log(`decoded token: ${JSON.stringify(decodedToken)}`);
-    return decodedToken;
-  } catch (err) {
-    console.warn(`invalid token received`);
-    return false;
-  }
-};
 
 export default async function handler(
   req: NextApiRequest,
---
+++ server/adminRequired.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import firebaseAdmin from './firebase/admin';
+
+const adminRequired = async (
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return false;
+  }
+  if (authorization.indexOf(`Bearer `) < 0) {
+    res.status(403).json({ error: 'Bearer token required' });
+    return false;
+  }
+  const token = authorization.replace('Bearer ', '');
+  try {
+    const decodedToken = await firebaseAdmin.auth().verifyIdToken(token);
+    // console.log(`decoded token: ${JSON.stringify(decodedToken)}`);
+    if (decodedToken.email !== 'paul.woelfel@ff-neusiedlamsee.at') {
+      res.status(403).json({ error: 'Forbidden' });
+      return false;
+    }
+    return decodedToken;
+  } catch (err) {
+    console.warn(`invalid token received`);
+    return false;
+  }
+};
+
+export default adminRequired;
