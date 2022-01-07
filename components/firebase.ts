c7ce8c4dfd46793430fa7b66095918552424556f
Paul Woelfel <paul@woelfel.at>
2022-01-07 21:12:07 +0100

    Diable firebase analytics

--- components/firebase.ts
+++ components/firebase.ts
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from 'firebase/app';
-import { Analytics, getAnalytics } from 'firebase/analytics';
-import { Firestore, getFirestore } from 'firebase/firestore';
+import { getApp, getApps, initializeApp } from 'firebase/app';
 import { Auth, getAuth } from 'firebase/auth';
+import { Firestore, getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,7 +15,7 @@ const firebaseConfig = JSON.parse(
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 export default app;
 export { app as firebaseApp };
-export const analytics: Analytics = getAnalytics(app);
+// export const analytics: Analytics = getAnalytics(app);
 export const firestore: Firestore = getFirestore(app);
 export const db = firestore;
 export const auth: Auth = getAuth(app);
