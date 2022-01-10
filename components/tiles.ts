2e7055b2a68fa3198581bd94874966d211001c28
Paul Woelfel <paul@woelfel.at>
2022-01-10 22:02:31 +0100

    Add openstreetmap

--- components/tiles.ts
+++ components/tiles.ts
@@ -60,6 +60,14 @@ export const availableLayers: TileConfigs = {
       ],
     },
   },
+  openstreetmap: {
+    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    options: {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  },
 };
 
 export const overlayLayers: TileConfigs = {
