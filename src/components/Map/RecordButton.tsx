6d2c87de325f04f0e5d847d30eed61dfc6e648ea
Paul Woelfel <paul@woelfel.at>
2024-01-18 09:02:42 +0100

    Add recording

---
+++ src/components/Map/RecordButton.tsx
@@ -0,0 +1,136 @@
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import { Tooltip } from '@mui/material';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import { useCallback, useEffect, useState } from 'react';
+import { LatLngPosition } from '../../common/geo';
+import { formatTimestamp } from '../../common/time-format';
+import { toLatLng } from '../../hooks/constants';
+import useFirecallItemAdd from '../../hooks/useFirecallItemAdd';
+import useFirecallItemUpdate from '../../hooks/useFirecallItemUpdate';
+import { calculateDistance } from '../FirecallItems/elements/connection/distance';
+import { Line } from '../firebase/firestore';
+import { usePositionContext } from './Position';
+import { LatLng } from 'leaflet';
+import { useMap } from 'react-leaflet';
+
+export default function RecordButton() {
+  const [isRecording, setIsRecording] = useState(false);
+  const [recordItem, setRecordItem] = useState<Line>();
+  const [positions, setPositions] = useState<LatLngPosition[]>([]);
+  const [timestamp, setTimestamp] = useState(new Date());
+
+  const [position, isPositionSet] = usePositionContext();
+  const map = useMap();
+  const updateFirecallItem = useFirecallItemUpdate();
+  const addFirecallItem = useFirecallItemAdd();
+
+  const addPos = useCallback(
+    async (newPos: LatLngPosition, record: Line) => {
+      if (recordItem?.id) {
+        // we need an id to update the item
+        const allPos: LatLngPosition[] = [
+          ...JSON.parse(record.positions || '[]'),
+          newPos,
+        ];
+        setPositions((p) => [...p, newPos]);
+        const newRecord: Line = {
+          ...record,
+          positions: JSON.stringify(allPos),
+          destLat: newPos[0],
+          destLng: newPos[1],
+          distance: calculateDistance(allPos),
+        };
+        await updateFirecallItem(newRecord);
+      }
+    },
+    [recordItem, updateFirecallItem]
+  );
+
+  const startRecording = useCallback(
+    async (pos: LatLng) => {
+      if (isPositionSet) {
+        map.setView(position);
+        const newRecord: Line = {
+          type: 'line',
+          name: `Track ${formatTimestamp(new Date())}`,
+          lat: pos.lat,
+          lng: pos.lng,
+          positions: JSON.stringify([[pos.lat, pos.lng]]),
+          destLat: pos.lat,
+          destLng: pos.lng,
+        };
+        setPositions([[pos.lat, pos.lng]]);
+        setRecordItem(newRecord);
+        addFirecallItem(newRecord);
+        setIsRecording(true);
+      }
+    },
+    [addFirecallItem, isPositionSet, map, position]
+  );
+
+  const stopRecording = useCallback(
+    async (pos: LatLng) => {
+      if (recordItem) {
+        addPos([pos.lat, pos.lng], recordItem);
+      }
+      setPositions([]);
+      setRecordItem(undefined);
+      setIsRecording(false);
+    },
+    [addPos, recordItem]
+  );
+
+  useEffect(() => {
+    if (isRecording && isPositionSet && recordItem && positions.length > 0) {
+      const lastPos = positions[positions.length - 1];
+      const distance = position.distanceTo(toLatLng(lastPos[0], lastPos[1]));
+
+      // more than 5m or 30 seconds
+      if (distance > 5 || (+new Date() - +timestamp) / 1000 > 30) {
+        map.setView(position);
+        addPos([lastPos[0], lastPos[1]], recordItem);
+      }
+    }
+  }, [
+    addPos,
+    isPositionSet,
+    isRecording,
+    map,
+    position,
+    positions,
+    recordItem,
+    timestamp,
+  ]);
+
+  return (
+    <Box
+      sx={{
+        // '& > :not(style)': { m: 1 },
+        position: 'absolute',
+        bottom: 96,
+        left: 16,
+      }}
+    >
+      {isPositionSet && (
+        <Tooltip title="GPS Track aufzeichnen">
+          <Fab
+            color={isRecording ? 'warning' : 'default'}
+            aria-label="add"
+            size="small"
+            onClick={(event) => {
+              event.preventDefault();
+              if (isRecording) {
+                stopRecording(position);
+              } else {
+                startRecording(position);
+              }
+            }}
+          >
+            <RadioButtonCheckedIcon />
+          </Fab>
+        </Tooltip>
+      )}
+    </Box>
+  );
+}
