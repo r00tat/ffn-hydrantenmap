7a9047310f3ba49e9b837b0eb18a089673f57b8a
Paul Woelfel <paul@woelfel.at>
2024-05-31 21:25:31 +0200

    Disable next-pwa

--- next.config.js
+++ next.config.js
@@ -1,39 +1,52 @@
+// @ts-check
+
 /** @type {import('next').NextConfig} */
-// const { InjectManifest } = require('workbox-webpack-plugin');
-
-const nextPWA = require('next-pwa');
-
-const withPWA = nextPWA({
+const nextConfig = {
+  /* config options here */
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  // disable: process.env.NODE_ENV === 'development',
   // disable: false,
   // register: true,
   // scope: '/app',
   sw: 'firebase-messaging-sw.js',
   skipWaiting: true,
-});
+};
 
-module.exports = withPWA({
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      },
-    ],
-  },
-  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-  //   if (!isServer) {
-  //     config.plugins.push(
-  //       new InjectManifest({
-  //         swSrc: './src/service-workers/firebase-messaging-sw.ts',
-  //         swDest: '../public/firebase-messaging-sw.js',
-  //         include: [],
-  //       })
-  //     );
-  //   }
+// const { InjectManifest } = require('workbox-webpack-plugin');
 
-  //   return config;
-  // },
-});
+// const nextPWA = require('next-pwa');
+
+// const withPWA = nextPWA({
+//   dest: 'public',
+//   disable: process.env.NODE_ENV === 'development',
+//   // disable: false,
+//   // register: true,
+//   // scope: '/app',
+//   sw: 'firebase-messaging-sw.js',
+//   skipWaiting: true,
+// });
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'firebasestorage.googleapis.com',
+//       },
+//     ],
+//   },
+//   // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//   //   if (!isServer) {
+//   //     config.plugins.push(
+//   //       new InjectManifest({
+//   //         swSrc: './src/service-workers/firebase-messaging-sw.ts',
+//   //         swDest: '../public/firebase-messaging-sw.js',
+//   //         include: [],
+//   //       })
+//   //     );
+//   //   }
+
+//   //   return config;
+//   // },
+// });
