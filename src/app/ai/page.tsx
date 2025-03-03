4ea8266c709e1488320a1fa26edbb3e20e96826c
Paul Woelfel <paul@woelfel.at>
2025-03-03 21:51:42 +0100

    Add progres indicator

--- src/app/ai/page.tsx
+++ src/app/ai/page.tsx
@@ -7,15 +7,20 @@ import { marked } from 'marked';
 import { useCallback, useState } from 'react';
 import firecallAIQuery from './aiQuery';
 import useFirecallSummary from './firecallSummary';
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function AiAssistantPage() {
   const [question, setQuestion] = useState('');
   const [answer, setAnswer] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const summary = useFirecallSummary();
   const askQuestion = useCallback(async () => {
+    setAnswer('');
+    setIsLoading(true);
     const answer = await firecallAIQuery(question, summary);
     const htmlText = await marked(answer);
     setAnswer(htmlText);
+    setIsLoading(false);
   }, [question, summary]);
 
   return (
@@ -31,18 +36,23 @@ export default function AiAssistantPage() {
         onChange={(e) => setQuestion(e.target.value)}
         fullWidth
         variant="outlined"
+        placeholder="Stelle Fragen oder Aufgaben zum Einsatz"
         onKeyDown={(e) => {
-          if (e.key === 'Enter') {
+          if (!isLoading && e.key === 'Enter') {
             askQuestion();
           }
         }}
       />
-      <Button variant="contained" color="primary" onClick={askQuestion}>
-        Ask
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={askQuestion}
+        disabled={isLoading}
+      >
+        Ask &nbsp; {isLoading && <CircularProgress color="primary" size={20} />}
       </Button>
 
       <Typography>
-        Antwort: <br />
         <span dangerouslySetInnerHTML={{ __html: answer }}></span>
       </Typography>
     </Paper>
