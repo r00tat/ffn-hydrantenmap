00c51d27e000e850bddc893ee40062f13c169487
Paul Woelfel <paul@woelfel.at>
2025-03-21 23:19:07 +0100

    Upgrade provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -17,11 +17,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5"
+      version = "~> 6"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 5"
+      version = "~> 6"
     }
   }
 }
