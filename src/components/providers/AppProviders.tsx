1e794d2b91ea3a0e8073116229bc76342e9103f7
Paul Woelfel <paul@woelfel.at>
2025-12-14 23:36:28 +0100

    fix: wrap AppProviders into a Suspense element

--- src/components/providers/AppProviders.tsx
+++ src/components/providers/AppProviders.tsx
@@ -1,9 +1,13 @@
 'use client';
 
+import CircularProgress from '@mui/material/CircularProgress';
 import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
 import { SessionProvider } from 'next-auth/react';
-import React from 'react';
+import dynamic from 'next/dynamic';
+import React, { Suspense } from 'react';
 import About from '../../app/about/page';
+import useFirebaseAppCheck from '../../hooks/useFirebaseAppCheck';
 import useFirebaseLogin from '../../hooks/useFirebaseLogin';
 import styles from '../../styles/Home.module.css';
 import SingedOutOneTapLogin from '../auth/SingedOutOneTapLogin';
@@ -14,9 +18,7 @@ import AppDrawer from '../site/AppDrawer';
 import HeaderBar from '../site/HeaderBar';
 import FirecallLayerProvider from './FirecallLayerProvider';
 import FirecallProvider from './FirecallProvider';
-import dynamic from 'next/dynamic';
 import MapEditorProvider from './MapEditorProvider';
-import useFirebaseAppCheck from '../../hooks/useFirebaseAppCheck';
 
 const DebugLoggingProvider = dynamic(() => import('./DebugLoggingProvider'), {
   ssr: false,
@@ -70,17 +72,25 @@ export default function AppProviders({ children }: AppProps) {
   useFirebaseAppCheck();
 
   return (
-    <SessionProvider>
-      <FirebaseUserProvider>
-        <DebugLoggingProvider>
-          <div className={styles.container}>
-            <CssBaseline enableColorScheme />
-            <SingedOutOneTapLogin />
+    <Suspense
+      fallback={
+        <Typography>
+          Loading ... <CircularProgress />
+        </Typography>
+      }
+    >
+      <SessionProvider>
+        <FirebaseUserProvider>
+          <DebugLoggingProvider>
+            <div className={styles.container}>
+              <CssBaseline enableColorScheme />
+              <SingedOutOneTapLogin />
 
-            <AuthorizationApp>{children}</AuthorizationApp>
-          </div>
-        </DebugLoggingProvider>
-      </FirebaseUserProvider>
-    </SessionProvider>
+              <AuthorizationApp>{children}</AuthorizationApp>
+            </div>
+          </DebugLoggingProvider>
+        </FirebaseUserProvider>
+      </SessionProvider>
+    </Suspense>
   );
 }
