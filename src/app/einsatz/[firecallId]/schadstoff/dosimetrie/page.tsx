82c5af0686f50303d1ec009c406db43492157ed5
Paul Woelfel <paul@woelfel.at>
2026-04-21 12:26:19 +0200

    fix(dosimetrie): Route unter /einsatz/[firecallId]/schadstoff/dosimetrie

---
+++ src/app/einsatz/[firecallId]/schadstoff/dosimetrie/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const Dosimetrie = dynamic(
+  () => import('../../../../../components/pages/Dosimetrie'),
+  {
+    ssr: false,
+    loading: () => null,
+  }
+);
+
+export default function DosimetriePage() {
+  return <Dosimetrie />;
+}
